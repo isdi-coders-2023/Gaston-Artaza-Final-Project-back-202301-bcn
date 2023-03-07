dcee32f845877590e261113e2fbc22a276357a62
Gaston82 <cabezaartaza@hotmail.com>
2023-03-07 22:14:08 +0100

    implement connectDatabase function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
+import "./loadEnvironment.js";
 import createDebug from "debug";
+import connectDatabase from "./database/connectDatabase.js";
+import chalk from "chalk";
+import startServer from "./server/startServer.js";
+
+export const debug = createDebug("users:*");
 
 const port = process.env.PORT ?? 4000;
-const mongoDdUrl = process.env.MONGODB_CONNECTION_URL;
+const mongoDbUrl = process.env.MONGODB_CONNECTION_URL;
+
+try {
+  await connectDatabase(mongoDbUrl!);
+  debug(chalk.green("Connected to data base"));
+
+  await startServer(+port);
+  debug(chalk.green(`Server listening on port ${port}`));
+} catch (error) {
+  debug(error.message);
+}
