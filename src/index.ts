f5de7f18ef93c8bac47e102cc46b1fccc9c9d76f
Gaston82 <cabezaartaza@hotmail.com>
2023-03-07 22:23:50 +0100

    Install morgan

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import connectDatabase from "./database/connectDatabase.js";
 import chalk from "chalk";
 import startServer from "./server/startServer.js";
 
-export const debug = createDebug("users:*");
+export const debug = createDebug("eventizi:*");
 
 const port = process.env.PORT ?? 4000;
 const mongoDbUrl = process.env.MONGODB_CONNECTION_URL;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,8 @@
 import express from "express";
+import morgan from "morgan";
 
 export const app = express();
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+app.use(express.json());
