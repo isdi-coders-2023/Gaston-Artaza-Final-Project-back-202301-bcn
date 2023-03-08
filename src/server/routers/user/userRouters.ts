cda8d04ccb0e76757a98b4be672c622d2d0a7a70
Gaston82 <cabezaartaza@hotmail.com>
2023-03-08 18:47:36 +0100

    Add loginUser validations

--- src/server/routers/user/userRouters.ts
+++ src/server/routers/user/userRouters.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import { loginUser } from "../../controllers/user/userControllers";
+import { check } from "express-validator";
 
 const userRouters = Router();
 
-userRouters.post("/login", loginUser);
+userRouters.post(
+  "/login",
+  [
+    check("username", "User name required").not().isEmpty(),
+    check("password", "Password required").not().isEmpty(),
+    check("password", "Password should have 6 characters minimun").isLength({
+      min: 6,
+    }),
+  ],
+  loginUser
+);
 
 export default userRouters;
